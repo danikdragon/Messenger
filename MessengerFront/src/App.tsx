8600299231d25669b846f8cabb4de09a792db62e
GatitoDeInvierno <89356559+danikdragon@users.noreply.github.com>
2026-07-03 02:07:34 +0700

    Ремонт проекта

--- MessengerFront/src/App.tsx
+++ MessengerFront/src/App.tsx
@@ -1,16 +1,34 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import '@/styles/globals.css';
+
+import TodoPage from './pages/todo';
+import NotFound from './pages/404';
+
+// 1. Компонент главной страницы (ваш текущий контент)
+function Home() {
+  const linksStyle = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out";
+  const links = [
+    { href: "/todo", label: "Список задач" },
+  ];
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-2">
+      <h1>Добро пожаловать!</h1>
+      {links.map(link => (
+        <Link key={link.href} className={linksStyle} to={link.href}>
+          {link.label}
+        </Link>
+      ))}
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/">Главная</Link>
-        <Link to="/todo">Список задач</Link>
-      </nav>
-
       <Routes>
-        <Route path="/todo" element={<h1>Список задач</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/todo" element={<TodoPage />} />
+        <Route path="*" element={<NotFound/>} />
       </Routes>
     </BrowserRouter>
   );
--- MessengerService/MessengerService/Program.cs
+++ MessengerService/MessengerService/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp", policy =>
     {
-        policy.WithOrigins("http://localhost:3000")
+        policy.WithOrigins("http://localhost:5173")
             .AllowAnyHeader()
             .AllowAnyMethod();
         // .AllowCredentials();
